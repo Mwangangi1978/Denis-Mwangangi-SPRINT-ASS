70da30cbadebc3457a36e06534e5c09add8e0dc8
mwangangi <dennomwangs1978@gmail.com>
2023-05-26 21:06:53 +0300

    added hamburger icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  content: ["./build/*.html","./build/js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'hamburger': {
+          '0%': {transform: 'scaleY(0)' },
+          '80%': {transform: 'scaleY(1.2)' },
+          '100%': {transform: 'scaleY(1)' },
+
+
+        },
+      },
+      animation: {
+        'hamburger': 'hamburger 0.5s ease 0.5s forwards ',
+      },
+    },
   },
   plugins: [],
 }
